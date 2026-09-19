71a5767bd7131b1cc0a940dace6c236f7befd621
Izuchukwu271 <echijioke483@gmail.com>
2026-09-19 18:36:37 +0100

    update server.js

--- server.js
+++ server.js
@@ -2,9 +2,12 @@ const express = require("express");
 const {Pool} = require("pg");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
+const cors = require("cors");
 require("dotenv").config();
 
 const app = express();
+
+app.use(cors());
 
 app.use(express.json());
 
